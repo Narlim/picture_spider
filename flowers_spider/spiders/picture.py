73796da535c324064ec2dea2f24bda6d8f27270f
Narlim <wangweimingooo@gmail.com>
2019-12-16 21:25:40 +0800

    format code

--- flowers_spider/spiders/picture.py
+++ flowers_spider/spiders/picture.py
@@ -7,21 +7,28 @@ from scrapy_splash import SplashRequest
 from scrapy.loader import ItemLoader
 from flowers_spider.items import FlowersSpiderItem
 
+
 class PictureSpider(scrapy.Spider):
     name = 'picture'
     allowed_domains = ['huaban.com']
-    
-    start_urls = ['https://huaban.com/favorite/beauty/?k457pe8h&max=2839114681&limit=20&wfl=1',]
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Accept': 'application/json', 'X-Request': 'JSON', 'X-Requested-With': 'XMLHttpRequest'}
 
-    
+    start_urls = [
+        'https://huaban.com/favorite/beauty/?k457pe8h&max=2839114681&limit=20&wfl=1',
+    ]
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'application/json',
+        'X-Request': 'JSON',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+
     def start_requests(self):
         for url in self.start_urls:
             yield Request(url=url, callback=self.parse, headers=self.headers)
-    
+
     def parse(self, response):
-        result_json = json.loads(response.text) 
+        result_json = json.loads(response.text)
         pins = result_json['pins']
         pin_list = [pins[pin_id]['pin_id'] for pin_id in range(len(pins))]
         last_pin = pin_list[-1]
@@ -32,12 +39,11 @@ class PictureSpider(scrapy.Spider):
             next_url = f'https://huaban.com/favorite/beauty/?k457pe8h&max={last_pin}&limit=20&wfl=1'
             yield Request(next_url, headers=self.headers, callback=self.parse)
 
-
     def pic_parse(self, response):
         l = ItemLoader(item=FlowersSpiderItem(), response=response)
         pic_url_div = response.xpath('//div[@class="image-holder"]')
-        image_urls = 'https:' + pic_url_div.xpath('.//img/@src').extract_first()
-        
-        l.add_value('image_urls', image_urls)
+        image_urls = 'https:' + pic_url_div.xpath(
+            './/img/@src').extract_first()
 
+        l.add_value('image_urls', image_urls)
         return l.load_item()
